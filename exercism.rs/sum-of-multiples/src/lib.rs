f1ab8b5b05da9d3e28019ebb1b524bf370a7254e
Minsub Kim <rapidfsub@gmail.com>
2023-10-29 23:13:26 +0900

    feat: solve exercism rs sum of multiples

--- exercism.rs/sum-of-multiples/src/lib.rs
+++ exercism.rs/sum-of-multiples/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    todo!("Sum the multiples of all of {factors:?} which are less than {limit}")
+    let mut result = 0;
+    for dividend in 1..limit {
+        for &divisor in factors {
+            if divisor > 0 && dividend % divisor == 0 {
+                result += dividend;
+                break;
+            }
+        }
+    }
+    result
 }
