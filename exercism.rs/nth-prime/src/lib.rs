f90ae7127ded5d1b9f600c4f31ad7938ea3b8f85
Minsub Kim <rapidfsub@gmail.com>
2023-10-29 18:03:06 +0900

    feat: solve exercism rs nth prime

--- exercism.rs/nth-prime/src/lib.rs
+++ exercism.rs/nth-prime/src/lib.rs
@@ -1,3 +1,27 @@
 pub fn nth(n: u32) -> u32 {
-    todo!("What is the 0-indexed {n}th prime number?")
+    let mut result = 2;
+    let mut i = 0;
+    loop {
+        if is_prime(result) {
+            if n == i {
+                return result;
+            } else {
+                i += 1;
+            }
+        }
+        result += 1;
+    }
+}
+
+fn is_prime(n: u32) -> bool {
+    for i in 2..=sqrt_floor(n) {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+fn sqrt_floor(n: u32) -> u32 {
+    (n as f64).powf(0.5).floor() as u32
 }
--- exercism.rs/nth-prime/tests/nth-prime.rs
+++ exercism.rs/nth-prime/tests/nth-prime.rs
@@ -6,19 +6,19 @@ fn first_prime() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn second_prime() {
     assert_eq!(np::nth(1), 3);
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn sixth_prime() {
     assert_eq!(np::nth(5), 13);
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn big_prime() {
     assert_eq!(np::nth(10_000), 104_743);
 }
