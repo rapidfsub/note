e2199d7307351cdccead037be40bf91b473ce257
Minsub Kim <rapidfsub@gmail.com>
2023-10-29 11:37:36 +0900

    feat: use graphmemes

--- exercism.rs/reverse-string/src/lib.rs
+++ exercism.rs/reverse-string/src/lib.rs
@@ -1,7 +1,9 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
     let mut result = String::new();
-    for letter in input.chars().rev() {
-        result.push(letter);
+    for letter in input.graphemes(true).rev() {
+        result.push_str(letter);
     }
     result
 }
