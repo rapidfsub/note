b3066fd8907b13e97056a9dfc9e9e0d3ceec17ba
Minsub Kim <rapidfsub@gmail.com>
2023-10-29 20:25:24 +0900

    feat: solve exercism rs prime factor

--- exercism.rs/prime-factors/src/lib.rs
+++ exercism.rs/prime-factors/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    todo!("This should calculate the prime factors of {n}")
+    let mut result = vec![];
+    let mut dividend = n;
+    let mut divisor = 2;
+    while dividend > 1 {
+        if dividend % divisor == 0 {
+            result.push(divisor);
+            dividend /= divisor;
+        } else {
+            divisor += 1;
+        }
+    }
+    result
 }
