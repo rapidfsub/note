cc341ec2ac9f36e03fd38f04420c997e8c965312
Minsub Kim <rapidfsub@gmail.com>
2023-10-29 21:15:54 +0900

    feat: solve exercism rs raindrops

--- exercism.rs/raindrops/src/lib.rs
+++ exercism.rs/raindrops/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn raindrops(n: u32) -> String {
-    todo!("what sound does Raindrop #{n} make?")
+    let result: String = [(3, "Pling"), (5, "Plang"), (7, "Plong")]
+        .iter()
+        .filter(|(divisor, _sound)| n % divisor == 0)
+        .map(|(_divisor, sound)| *sound)
+        .collect();
+    if result.is_empty() {
+        n.to_string()
+    } else {
+        result
+    }
 }
