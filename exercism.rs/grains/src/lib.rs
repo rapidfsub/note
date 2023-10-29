b5074f704f9cfda5eccbd2564d758efc5c7d26d0
Minsub Kim <rapidfsub@gmail.com>
2023-10-29 17:39:39 +0900

    feat: solve exercism rs grains

--- exercism.rs/grains/src/lib.rs
+++ exercism.rs/grains/src/lib.rs
@@ -1,7 +1,11 @@
 pub fn square(s: u32) -> u64 {
-    todo!("grains of rice on square {s}");
+    if 1 <= s && s <= 64 {
+        2_u64.pow(s - 1)
+    } else {
+        panic!("Square must be between 1 and 64")
+    }
 }
 
 pub fn total() -> u64 {
-    todo!();
+    (1..=64).map(square).sum()
 }
