67dcce780048b005e3c5727b2a175d5e7bdadbd9
Minsub Kim <rapidfsub@gmail.com>
2023-10-30 11:58:55 +0900

    feat: compare the guess to the secret number

--- the-rust-programming-language/guessing_game/src/main.rs
+++ the-rust-programming-language/guessing_game/src/main.rs
@@ -1,5 +1,5 @@
 use rand::Rng;
-use std::io;
+use std::{cmp::Ordering, io};
 
 fn main() {
     println!("Guess the number!");
@@ -11,5 +11,12 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-    println!("You guessed: {guess}")
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    println!("You guessed: {guess}");
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
