08f947c71dcfad128beed747dabaad1702473d72
Minsub Kim <rapidfsub@gmail.com>
2023-11-01 00:37:03 +0900

    feat: solve exercism rs matching brackets

--- exercism.rs/matching-brackets/src/lib.rs
+++ exercism.rs/matching-brackets/src/lib.rs
@@ -1,3 +1,67 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    todo!("Check if the string \"{string}\" contains balanced brackets");
+    let mut stack = Vec::new();
+    for bracket in string.chars().filter_map(Bracket::new) {
+        match bracket.open_close {
+            OpenClose::Open => stack.push(bracket),
+            OpenClose::Close => match stack.pop().map(|last| last.is_matching(&bracket)) {
+                Some(true) => continue,
+                _ => return false,
+            },
+        }
+    }
+    stack.is_empty()
+}
+
+#[derive(PartialEq)]
+enum BracketStyle {
+    Round,
+    Square,
+    Curly,
+}
+
+#[derive(PartialEq)]
+enum OpenClose {
+    Open,
+    Close,
+}
+
+struct Bracket {
+    style: BracketStyle,
+    open_close: OpenClose,
+}
+
+impl Bracket {
+    fn new(letter: char) -> Option<Self> {
+        match letter {
+            '(' => Some(Bracket {
+                style: BracketStyle::Round,
+                open_close: OpenClose::Open,
+            }),
+            ')' => Some(Bracket {
+                style: BracketStyle::Round,
+                open_close: OpenClose::Close,
+            }),
+            '[' => Some(Bracket {
+                style: BracketStyle::Square,
+                open_close: OpenClose::Open,
+            }),
+            ']' => Some(Bracket {
+                style: BracketStyle::Square,
+                open_close: OpenClose::Close,
+            }),
+            '{' => Some(Bracket {
+                style: BracketStyle::Curly,
+                open_close: OpenClose::Open,
+            }),
+            '}' => Some(Bracket {
+                style: BracketStyle::Curly,
+                open_close: OpenClose::Close,
+            }),
+            _ => None,
+        }
+    }
+
+    fn is_matching(&self, other: &Self) -> bool {
+        self.style == other.style && self.open_close != other.open_close
+    }
 }
