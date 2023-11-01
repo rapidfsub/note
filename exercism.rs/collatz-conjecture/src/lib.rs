dd4c3c4659a9416cacd79c1c7c4a90c8d26ecc9c
Minsub Kim <rapidfsub@gmail.com>
2023-11-01 09:52:53 +0900

    feat: solve exercism rs collatz conjecture

--- exercism.rs/collatz-conjecture/src/lib.rs
+++ exercism.rs/collatz-conjecture/src/lib.rs
@@ -1,3 +1,21 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    todo!("return Some(x) where x is the number of steps required to reach 1 starting with {n}")
+    if n < 1 {
+        return None;
+    }
+    let mut n = n;
+    let mut count = 0;
+    loop {
+        if n == 1 {
+            break;
+        } else if n % 2 == 0 {
+            n /= 2;
+            count += 1;
+        } else {
+            match n.checked_mul(3).and_then(|n| n.checked_add(1)) {
+                Some(result) => (n, count) = (result, count + 1),
+                None => return None,
+            }
+        }
+    }
+    Some(count)
 }
