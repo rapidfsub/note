3688df69f9c366428697dc33dab56762e17817b6
Minsub Kim <rapidfsub@gmail.com>
2023-11-06 14:31:21 +0900

    feat: solve exercism rs diffie hellman

--- exercism.rs/diffie-hellman/src/lib.rs
+++ exercism.rs/diffie-hellman/src/lib.rs
@@ -1,11 +1,24 @@
+use rand::seq::IteratorRandom;
+use rand::thread_rng;
+
 pub fn private_key(p: u64) -> u64 {
-    todo!("Pick a private key greater than 1 and less than {p}")
+    (2..p).choose(&mut thread_rng()).unwrap()
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    todo!("Calculate public key using prime numbers {p} and {g}, and private key {a}")
+    exp_mod(g as u128, a, p as u128) as u64
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    todo!("Calculate secret key using prime number {p}, public key {b_pub}, and private key {a}")
+    exp_mod(b_pub as u128, a, p as u128) as u64
+}
+
+fn exp_mod(base: u128, exp: u64, divisor: u128) -> u128 {
+    if exp == 0 {
+        1
+    } else if exp % 2 == 0 {
+        (exp_mod(base, exp / 2, divisor).pow(2)) % divisor
+    } else {
+        (base * exp_mod(base, exp - 1, divisor)) % divisor
+    }
 }
