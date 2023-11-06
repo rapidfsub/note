8d3ee9f2ccc0425e875dc8adb17b57cccab0222e
Minsub Kim <rapidfsub@gmail.com>
2023-11-06 15:08:00 +0900

    refactor: use while loop and bitwise operators

--- exercism.rs/diffie-hellman/src/lib.rs
+++ exercism.rs/diffie-hellman/src/lib.rs
@@ -1,8 +1,7 @@
-use rand::seq::IteratorRandom;
-use rand::thread_rng;
+use rand::{thread_rng, Rng};
 
 pub fn private_key(p: u64) -> u64 {
-    (2..p).choose(&mut thread_rng()).unwrap()
+    thread_rng().gen_range(2..p)
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
@@ -13,12 +12,14 @@ pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
     exp_mod(b_pub as u128, a, p as u128) as u64
 }
 
-fn exp_mod(base: u128, exp: u64, divisor: u128) -> u128 {
-    if exp == 0 {
-        1
-    } else if exp % 2 == 0 {
-        (exp_mod(base, exp / 2, divisor).pow(2)) % divisor
-    } else {
-        (base * exp_mod(base, exp - 1, divisor)) % divisor
+fn exp_mod(mut base: u128, mut exp: u64, divisor: u128) -> u128 {
+    let mut result = 1;
+    while exp > 0 {
+        if exp & 1 > 0 {
+            result = result * base % divisor;
+        }
+        base = base.pow(2) % divisor;
+        exp = exp >> 1;
     }
+    result
 }
