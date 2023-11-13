942cd55690da16d23f622680b596c4d97a6ae59f
Minsub Kim <rapidfsub@gmail.com>
2023-11-13 10:02:30 +0900

    feat: store strings in an array

--- hands-on-rust-herbert-wolverson/treehouse/src/main.rs
+++ hands-on-rust-herbert-wolverson/treehouse/src/main.rs
@@ -2,9 +2,21 @@ use std::io::stdin;
 
 fn main() {
     println!("Hello, what's your name?");
-    let your_name = what_is_your_name();
-    println!("Hello, {:?}", your_name)
+    let visitor_list = ["bert", "steve", "fred"];
+    let name = what_is_your_name();
+    let mut allow_them_in = false;
+    for visitor in &visitor_list {
+        if visitor == &name {
+            allow_them_in = true;
+        }
+    }
+    if allow_them_in {
+        println!("Welcome to the Treehouse, {}", name);
+    } else {
+        println!("Sorry, you aren't on the list.");
+    }
 }
+
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
     stdin()
