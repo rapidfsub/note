77898a7474b61b3e200eaaadeb1c15ebff17dce8
Minsub Kim <rapidfsub@gmail.com>
2023-11-15 13:42:38 +0900

    feat: implement combat

--- dungeon_crawler/src/systems/player_input.rs
+++ dungeon_crawler/src/systems/player_input.rs
@@ -19,14 +19,32 @@ pub fn player_input(
         };
         if delta.x != 0 || delta.y != 0 {
             let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
-            players.iter(ecs).for_each(|(entity, pos)| {
+            let (player_entity, destination) = players
+                .iter(ecs)
+                .map(|(entity, pos)| (*entity, *pos + delta))
+                .next()
+                .unwrap();
+            let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+            let mut hit_something = false;
+            enemies
+                .iter(ecs)
+                .filter(|(_, pos)| **pos == destination)
+                .for_each(|(entity, _)| {
+                    hit_something = true;
+                    let command = WantsToAttack {
+                        attacker: player_entity,
+                        victim: *entity,
+                    };
+                    commands.push(((), command));
+                });
+            if !hit_something {
                 let wants_to_move = WantsToMove {
-                    entity: *entity,
-                    destination: *pos + delta,
+                    entity: player_entity,
+                    destination,
                 };
                 commands.push(((), wants_to_move));
-            });
-            *turn_state = TurnState::PlayerTurn;
+            }
         }
+        *turn_state = TurnState::PlayerTurn;
     }
 }
