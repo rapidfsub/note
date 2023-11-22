a4be39deb6259d706ad9003b511455be598ab657
Minsub Kim <rapidfsub@gmail.com>
2023-11-22 15:05:25 +0900

    feat: track game level

--- dungeon_crawler/src/systems/end_turn.rs
+++ dungeon_crawler/src/systems/end_turn.rs
@@ -5,7 +5,7 @@ use crate::prelude::*;
 #[read_component(Player)]
 #[read_component(Point)]
 #[read_component(AmuletOfYala)]
-pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState) {
+pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState, #[resource] map: &Map) {
     let mut player_hp = <(&Health, &Point)>::query().filter(component::<Player>());
     let mut amulet = <&Point>::query().filter(component::<AmuletOfYala>());
     let amulet_default = Point::new(-1, -1);
@@ -20,8 +20,13 @@ pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState) {
     player_hp.iter(ecs).for_each(|(hp, pos)| {
         if hp.current < 1 {
             new_state = TurnState::GameOver;
-        } else if pos == amulet_pos {
+        }
+        if pos == amulet_pos {
             new_state = TurnState::Victory;
+        }
+        let idx = map.point2d_to_index(*pos);
+        if map.tiles[idx] == TileType::Exit {
+            new_state = TurnState::NextLevel;
         }
     });
     *turn_state = new_state;
